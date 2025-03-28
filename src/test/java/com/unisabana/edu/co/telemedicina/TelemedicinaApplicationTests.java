10aefa481c5c458d35deae017a6ca255a3c42459
Samuel Espitia <samuelespitia88@gmail.com>
2025-03-28 12:29:29 -0500

    first commit

---
+++ src/test/java/com/unisabana/edu/co/telemedicina/TelemedicinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisabana.edu.co.telemedicina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedicinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
